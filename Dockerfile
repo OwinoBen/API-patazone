19de25f55af363deb38acce0ad0d7ff31bcf47ad
OwinoBen <bensonowinoopondo@gmail.com>
2023-05-17 19:40:07 +0300

    new

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
-FROM python:3
+FROM python:3.9.6-alpine
 WORKDIR /ecommerce_api_project
+
+#settting environment variables
+ENV PYTHONDONTWRITEBYTECODE 1 #Prevents Python from writing pyc files to disc
+ENV PYTHONUNBUFFERED 1 #Prevents Python from buffering stdout and stderr
+
+#install dependancies
+RUN pip install --upgrade pip
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
+
+#copy project
 COPY . .
--- api/settings.py
+++ api/settings.py
@@ -23,14 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-# print(os.environ.get('SECRET_KEY'))
-
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # turning false to allow error 500 displayed as json
+# DEBUG = True  # turning false to allow error 500 displayed as json
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 APPEND_SLASH = False  # REMOVE THE TRAILING SLASH IN URL e.g http://127.0.0.1:8000/
 
